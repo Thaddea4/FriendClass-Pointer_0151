6bdc87a4a6c72445c7c22be7436921581b64a475
Thaddea4 <adeyasyie@gmail.com>
2026-04-27 20:12:01 +0700

    Menampikan teks tentang layang layang

--- tugas.cpp
+++ tugas.cpp
@@ -12,3 +12,8 @@ private:
 
 public:
     void inputData(){
+        cout << "Layang-Layang\n";
+        cout << "Masukkan diagonal 1: "; cin >> d1;
+        cout << "Masukkan diagonal 2: "; cin >> d2;
+        cout << "Masukkan sisi: "; cin >> sisi;
+    }
