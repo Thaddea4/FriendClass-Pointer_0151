28eabee21c5c0291e6d2ace3fbd21b333e37955f
Thaddea4 <adeyasyie@gmail.com>
2026-04-27 16:07:01 +0700

    Menambahkan friend function

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -6,3 +6,12 @@ class mahasiswa
 {
     private:
     string nama;
+    public:
+    friend void setnama(mahasiswa &a, string);
+};
+
+void setnama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
